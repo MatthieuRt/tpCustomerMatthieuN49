c3357e3d89cab573aae7257a95f2d45fa6c1ce0d
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-03-07 20:54:47 +0300

    Ajout de la configuration pour JSF

--- src/main/java/mg/itu/tpcustomermatthieun49/service/CustomerManager.java
+++ src/main/java/mg/itu/tpcustomermatthieun49/service/CustomerManager.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package service;
+package mg.itu.tpcustomermatthieun49.service;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.EntityManager;
